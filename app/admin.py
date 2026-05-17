a9e8120da5b9c9b5d469b3d9164985c9c9faf09c
Luiz Francisco <luizinhofrancisco9889@gmail.com>
2026-05-17 16:51:02 -0300

    feat: adiciona modelos Plano, Modalidade, Treino, Pagamento e registra no admin

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-
-from .models import Aluno
+from .models import Aluno, Plano, Modalidade, Treino, Pagamento, Instrutor
 
 admin.site.register(Aluno)
+admin.site.register(Plano)
+admin.site.register(Modalidade)
+admin.site.register(Treino)
+admin.site.register(Pagamento)
+admin.site.register(Instrutor)
