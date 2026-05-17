cef87526e707599b71b9a6624d72e29868140f8d
Julia Morais <eijuliamorais@gmail.com>
2026-05-17 15:23:07 -0300

    add: criação dos models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Plano(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    duracao_meses = models.IntegerField()
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+class Instrutor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=20)
+    especialidade = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Modalidade(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+class Treino(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    instrutor = models.ForeignKey(
+        Instrutor,
+        on_delete=models.CASCADE
+    )
+    modalidade = models.ForeignKey(
+        Modalidade,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.nome
+
+
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -11,3 +52,4 @@ class Aluno(models.Model):
 
     def __str__(self):
         return self.nome
+
