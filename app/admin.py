632b9ea7794a6746561a25a631f33c817d43c124
Julia Morais <eijuliamorais@gmail.com>
2026-05-17 17:49:46 -0300

    feat: customização do admin

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,103 @@
 from django.contrib import admin
-from .models import Aluno, Plano, Modalidade, Treino, Pagamento, Instrutor
+from .models import *
 
-admin.site.register(Aluno)
-admin.site.register(Plano)
-admin.site.register(Modalidade)
-admin.site.register(Treino)
-admin.site.register(Pagamento)
-admin.site.register(Instrutor)
+
+class PagamentoInline(admin.TabularInline):
+    model = Pagamento
+    extra = 1
+
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+        'email',
+        'telefone',
+        'idade',
+        'ativo',
+        'plano',
+    )
+
+    search_fields = (
+        'nome',
+        'email',
+        'telefone',
+    )
+
+    list_filter = (
+        'ativo',
+        'plano',
+    )
+
+    inlines = [PagamentoInline]
+
+
+@admin.register(Plano)
+class PlanoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+        'preco',
+        'duracao_meses',
+    )
+
+    search_fields = (
+        'nome',
+    )
+
+
+@admin.register(Instrutor)
+class InstrutorAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+        'email',
+        'especialidade',
+    )
+
+    search_fields = (
+        'nome',
+        'especialidade',
+    )
+
+
+@admin.register(Treino)
+class TreinoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+        'instrutor',
+        'modalidade',
+    )
+
+    list_filter = (
+        'modalidade',
+    )
+
+    search_fields = (
+        'nome',
+    )
+
+
+@admin.register(Modalidade)
+class ModalidadeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+    )
+
+
+@admin.register(Pagamento)
+class PagamentoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'aluno',
+        'valor',
+        'status',
+        'data_pagamento',
+    )
+
+    list_filter = (
+        'status',
+    )
